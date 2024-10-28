e2c98dc5848b3bba481a7bf047b5ac3a61a037b2
Felipe Silva <a25198@esenviseu.net>
2024-10-28 16:33:21 +0000

    a

--- letras10.py
+++ letras10.py
@@ -1,8 +1,10 @@
-contar=0
-for i in range(10):
-    letras=input("Introduza uma letra:")
-
-    for frase in letras:
-        if frase in "aeiouAEIOU":
-            contar=contar+1
-print("A frase indicada tem",contar,"vogais")
+contar_vogais=0
+for _ in range(10):
+    letra=input("Introduza uma letra:")
+    letra=letra.strip() #remover os espaços em branco no inicio e final da string
+    if len(letra) != 1:
+        print("Só pode inserir uma letra")
+    else:
+        if letra in "aeiouAEIOU":
+            contar_vogais=contar_vogais+1
+print("A frase indicada tem",contar_vogais,"vogais")
