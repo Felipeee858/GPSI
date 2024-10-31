efce0ab93481ef03e15022b2fa42de4dd063af81
Felipe Silva <a25198@esenviseu.net>
2024-10-31 13:07:00 +0000

    a

--- meses_anos2.py
+++ meses_anos2.py
@@ -8,7 +8,7 @@ for mes in range(mes_atual,13):
     if mes == 4 or mes==5 or mes== 9 or mes ==11:
         dias_mes=30
     elif mes == 2:
-        if (ano % 4 == 0 ans ano % 100 !=0) or ano % 400=0:
+        if (ano % 4 == 0 and ano % 100 !=0) or ano % 400==0:
             dias_mes = 30
         else:
             dias_mes = 28
